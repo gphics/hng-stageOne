519f8115e3411a51c98aa2077c2411c626dd873b
gphics <97003589+gphics@users.noreply.github.com>
2023-09-07 23:42:19 +0100

    created the endpoint

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+//   console.log();
+  const dateArr = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const current_day = dateArr[date.getDay()];
+  const { slack_name, track } = req.query;
+  res.status(200).json({
+    slack_name,
+    current_day,
+    track,
+    utc_time: date.toISOString(),
+    status_code: 200,
+  });
+});
+
+app.listen(7000);
